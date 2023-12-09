86d12e7df65f29b9e34e1030496a3a5cc17f9323
caengle <120054043+caengle@users.noreply.github.com>
2023-12-09 17:34:10 -0600

    Update advent03-2.py

--- advent03-2.py
+++ advent03-2.py
@@ -2,63 +2,83 @@
 
 import re
 
-def get_coordinates(line):
-    coordinates = []
-    pattern = re.compile(r'\d+')
-    numbers = pattern.findall(line)
-    x_start = 0
-    num = ''
-    curr = '.'
-    prev = '.'
+def get_num(engine, x, y):
+    num = engine[y][x]
+    i = x
+    while i > 0:
+        i-=1
+        curr = engine[y][i]
+        if curr.isdigit():
+            num = curr+num
+        else:
+            break
+    i = x
+    while i < len(engine[0])-1:
+        i+=1
+        curr = engine[y][i]
+        if curr.isdigit():
+            num = num + curr
+        else:
+            break
+    return int(num)
 
-    for i in range(len(line)):
-        curr = line[i]
-        if not prev.isdigit() and curr.isdigit():
-            x_start = i
-            if i == len(line) - 1:
-                coordinates.append((x_start, i))
-        elif prev.isdigit() and curr.isdigit():
-            num += curr
-            if i == len(line) - 1:
-                coordinates.append((x_start, i))
-        elif prev.isdigit() and not curr.isdigit():
-            coordinates.append((x_start, i - 1))
-        prev = curr
-    
-    return(numbers, coordinates)
+def check_adjacent_numbers(engine, x, y):
+    adjacent_numbers = []
+    x0 = x
+    x1 = x
+    y0 = y
+    y1 = y
+    if x > 0:
+        x0 = x - 1
+    if y > 0:
+        y0 = y - 1
+    if x < len(engine[0]) - 1:
+        x1 = x + 1
+    if y < len(engine) - 1:
+        y1 = y + 1
 
-def get_gear_ratio(num_coor, x, y, width):
-    ratio = 0
-    nums = []
+    prev_num = False
+    if y != 0:
+        for i in range(x0, x1+1):
+            print(x,y,engine[y0][i])
+            if engine[y0][i].isdigit():
+                if prev_num == False:
+                    adjacent_numbers.append((i,y0))
+                    prev_num = True
+            else:
+                prev_num = False
 
-    for i in range(0, len(num_coor[y])):
-        if (y > 0 and x >= num_coor[y-1][1][i][0] - 1 and x <= num_coor[y-1][1][i][1] + 1):
-            nums.append(num_coor[y-1][0][i])
-        if (y < len(num_coor) - 1 and x >= num_coor[y+1][1][i][0] - 1 and x <= num_coor[y+1][1][i][1] + 1):
-            nums.append(num_coor[y+1][0][i])
-        if (x > 0 and y == i and x == num_coor[y][1][i][0] - 1) or (x < width - 1 and x > 0 and y == i and x == num_coor[y][1][i][0] + 1):
-            nums.append(num_coor[y+1][0][i])
-    if len(nums) == 2:
-        ratio = int(nums[0]) * int(nums[1])
-    return ratio
+    prev_num = False                
+    if y != len(engine) - 1:
+        for i in range(x0, x1+1):
+            if engine[y1][i].isdigit():
+                if prev_num == False:
+                    adjacent_numbers.append((i,y1))
+                    prev_num = True
+            else:
+                prev_num = False
+                
+    if x != 0:
+        if engine[y][x0].isdigit():
+            adjacent_numbers.append((x0,y))
+    if x != len(engine[0]) - 1:
+        if engine[y][x1].isdigit():
+            adjacent_numbers.append((x1,y))
+    return(adjacent_numbers)    
 
 def main():
     sum = 0
     f = open("test.txt", "r")
     engine = f.readlines()
-    num_coor = []
-    #[([10, 20, 30], [(1, 2), (3, 4)]), ([10, 20, 30], [(1, 2), (3, 4)])]
-    for i in range(0, len(engine)):
+    for i in range(len(engine)):
         engine[i] = engine[i].strip()
-    for i in range(0, len(engine)):
-        num_coor.append(get_coordinates(engine[i]))
-    print(num_coor)
-    for y in range(0, len(engine)):
-        for x in range(0, len(engine[y])):
-            #print(x,y)
-            if re.search(r'\*', engine[y][x]) != None:
-                sum += get_gear_ratio(num_coor, x, y, len(engine[0]))
-
+        for j in range(len(engine[i])):
+            if engine[i][j] == '*':
+                #print(j,i)
+                adjacent_numbers = check_adjacent_numbers(engine, j, i)
+                if len(adjacent_numbers) == 2:
+                    print(adjacent_numbers)
+                    sum += get_num(engine, adjacent_numbers[0][0], adjacent_numbers[0][1]) * get_num(engine, adjacent_numbers[1][0], adjacent_numbers[1][1])
     print(sum)
 
 if __name__ == "__main__":
